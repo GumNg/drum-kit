26a5ad952bb6510f9a419b6751370be1989814f8
Gum Ng <jon9609@gmail.com>
2022-07-28 14:01:03 -0700

    Added new audio object variable and method calling the new variable to play audio

---
+++ index.js
@@ -0,0 +1,15 @@
+// sets variable to total length of .drum buttons
+let numOfDrumBtn = document.querySelectorAll(".drum").length;
+
+// sets for loop to run and incriment if i is less than the length of variable
+for (let i = 0; i < numOfDrumBtn; i++) {
+
+// query selects all .drum classes incriments with i through each loop and calls anonymous function
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+        // adds new variable for new audio object and selects file
+        let tom1 = new Audio(`sounds/tom-1.mp3`);
+        // calls the new object to play sound
+        tom1.play();
+    });
+};
