f4c7addddb666e3c6117686a62ba62489b54f901
Gum Ng <jon9609@gmail.com>
2022-07-28 16:26:36 -0700

    added switch with first case being w to play tom1 audio

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ let numOfDrumBtn = document.querySelectorAll(".drum").length;
 
 // sets for loop to run and incriment if i is less than the length of variable
 for (let i = 0; i < numOfDrumBtn; i++) {
+  // query selects all .drum classes incriments with i through each loop and calls anonymous function
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    // creates a variable to hold this and the html content inside
+    let btnInnerHtml = this.innerHTML;
 
-// query selects all .drum classes incriments with i through each loop and calls anonymous function
-    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        
-        // creates a variable to hold this and the html content inside
-        let btnInnerHtml = this.innerHTML;
-
-
+    switch (btnInnerHtml) {
+      case "w":
         // adds new variable for new audio object and selects file
         let tom1 = new Audio(`sounds/tom-1.mp3`);
         // calls the new object to play sound
         tom1.play();
-    });
-};
+        break;
+        default:
+            
+    }
+  });
+}
